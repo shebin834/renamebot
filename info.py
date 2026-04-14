2b1368e050c35f8a5b51570fd228232e0e75d8be
shebin k <shebinkallankunnan@gmail.com>
2026-04-14 22:10:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
+BOT_TOKEN = environ['8728203307:AAGIgqWDXACPSbz3P_XswV8TwepAzPX15nA']
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
