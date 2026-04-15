78ece20bb055ec3d1e05d4d3adf18aa0eedcbd17
shebin k <shebinkallankunnan@gmail.com>
2026-04-15 10:15:16 +0530

    Fix shebang and improve variable quoting in start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,14 @@
-if [ -z $UPSTREAM_REPO ]
+#!/bin/bash
+
+if [ -z "$UPSTREAM_REPO" ]
 then
   echo "Cloning main Repository"
   git clone https://github.com/lazyindu/test1.git /LazyPrincess
 else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  echo "Cloning Custom Repo from $UPSTREAM_REPO"
   git clone $UPSTREAM_REPO /LazyPrincess
 fi
+
 cd /LazyPrincess
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
